2ebe0041188e3af00d03387ea25d092242782daf
tpyung <160987551+tpyung@users.noreply.github.com>
2024-04-19 11:42:47 +0800

    0419

---
+++ ob/data.js
@@ -0,0 +1,248 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "觀景臺1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.9314895683118749,
+        "pitch": 0.15175136340195294,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.00010154918416382941,
+          "pitch": 0.12994970430007946,
+          "rotation": 0,
+          "target": "3-4"
+        },
+        {
+          "yaw": 1.5589331178843775,
+          "pitch": 0.19753215660208845,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.590035058837036,
+          "pitch": -0.027821083290366033,
+          "title": "觀景台",
+          "text": "<div><br></div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "觀景臺2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.19946620022792416,
+        "pitch": -0.025291893900327267,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9910874711987674,
+          "pitch": 0.24612666336850708,
+          "rotation": 6.283185307179586,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05418662524388651,
+          "pitch": -0.2615757346706147,
+          "title": "觀景台",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "觀景臺3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 0.38475182435151467,
+        "pitch": 0.04518329608695737,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.898922338505418,
+          "pitch": 0.25783494580994315,
+          "rotation": 7.0685834705770345,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.28266481358993545,
+          "pitch": 0.45799397701312117,
+          "title": "觀景台",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-4",
+      "name": "觀景臺4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.11932353049348876,
+        "pitch": 0.15932624735397738,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1377742459170808,
+          "pitch": 0.3301160425105891,
+          "rotation": 12.566370614359176,
+          "target": "0-1"
+        },
+        {
+          "yaw": -0.5859319631695215,
+          "pitch": 0.577597465889852,
+          "rotation": 3.141592653589793,
+          "target": "4-5"
+        },
+        {
+          "yaw": -0.07087073292386492,
+          "pitch": 0.07797243446779234,
+          "rotation": 1.5707963267948966,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3614143558447598,
+          "pitch": 0.3629779848202155,
+          "title": "觀景台",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-5",
+      "name": "觀景臺5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.4723329046249951,
+        "pitch": 0.17957244669232253,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06093515820390216,
+          "pitch": 0.7133621073454837,
+          "rotation": 0,
+          "target": "3-4"
+        },
+        {
+          "yaw": 1.2276195102365257,
+          "pitch": 0.1491894843270991,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0034659869229987095,
+          "pitch": 0.39410730682603656,
+          "title": "觀景台",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "觀景台",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
