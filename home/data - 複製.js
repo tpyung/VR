3c5abb00cd1308fe67509fc5e96b23dcefda46e7
tpyung <160987551+tpyung@users.noreply.github.com>
2024-05-03 10:40:26 +0800

    0503

---
+++ home/data - 複製.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "首頁1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.11809472507716912,
+        "pitch": -0.3970827342351164,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.07030751059914664,
+          "pitch": 0.08907416678536606,
+          "title": "松山機場入口",
+          "text": "10548台北市松山區敦化北路340-9號"
+        }
+      ]
+    }
+  ],
+  "name": "首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
